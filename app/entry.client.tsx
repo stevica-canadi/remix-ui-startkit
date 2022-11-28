7305193d24ef573d7ad8042714d1cc17458c1b39
Stevica Canadi <steve@fieldmade.no>
2022-11-28 20:24:52 +0100

    Add styled-components

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,22 +1,10 @@
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { RemixBrowser } from '@remix-run/react';
+import { hydrateRoot } from 'react-dom/client';
+import { useLocation, useMatches } from '@remix-run/react';
+import { useEffect } from 'react';
 
-function hydrate() {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
-  });
+function Client() {
+    return <RemixBrowser />;
 }
 
-if (window.requestIdleCallback) {
-  window.requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  window.setTimeout(hydrate, 1);
-}
+hydrateRoot(document, <Client />);
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,58 @@
-import { Button } from "@fieldmade/ui";
+import { Button, Select } from "@fieldmade/ui";
 
 export default function Index() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex flex-col justify-center items-center space-y-12">
       <Button variation="primary">Hello World</Button>
+      <Select
+        className="w-64"
+      options={[
+        {
+          label: 'Yellow',
+          value: 'yellow'
+        },
+        {
+          label: 'Blue',
+          value: 'blue'
+        },
+        {
+          label: 'Green',
+          value: 'green'
+        },
+        {
+          label: 'Pink',
+          value: 'pink'
+        },
+        {
+          label: 'Purple',
+          value: 'purple'
+        },
+        {
+          label: 'Orange',
+          value: 'orange'
+        },
+        {
+          label: 'Gray',
+          value: 'gray'
+        },
+        {
+          label: 'Indigo',
+          value: 'indigo'
+        },
+        {
+          label: 'White',
+          value: 'white'
+        },
+        {
+          label: 'Magenta',
+          value: 'magenta'
+        }
+      ]}
+      value={{
+        label: 'Yellow',
+        value: 'yellow'
+      }}
+    />
     </div>
   );
 }
